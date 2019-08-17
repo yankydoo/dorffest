57ca1192436c92bf1b9773c10f3b1229e06f0597
Michael Jank <michael.jank@live.at>
2019-08-17 02:00:58 +0200

    Updates

---
+++ precache-manifest.ca47f9a9a16685548aa5bb3a5bba09dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb33451fee3ff717332671cd6a735b1b",
+    "url": "/rechna/index.html"
+  },
+  {
+    "revision": "14994900c4406dd086a9",
+    "url": "/rechna/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "14dffdd9a19442be7c62",
+    "url": "/rechna/static/js/2.c1258383.chunk.js"
+  },
+  {
+    "revision": "14994900c4406dd086a9",
+    "url": "/rechna/static/js/main.60bbab4c.chunk.js"
+  },
+  {
+    "revision": "904fa56e9ca7e619a9b8",
+    "url": "/rechna/static/js/runtime~main.29b9074f.js"
+  }
+]);
